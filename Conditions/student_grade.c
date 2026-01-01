5ff5ab2155f350501418a531da68a2d73d70c06b
divya-patil-CS <divyaoffcial33@gmail.com>
2026-01-01 23:37:16 +0530

    Create student_grade.c

---
+++ Conditions/student_grade.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+   int marks;
+    printf("Enter marks (0-100): ");
+    scanf("%d", &marks);
+    if (marks >= 90)
+        printf("Grade: A+");
+    else if (marks >= 80)
+        printf("Grade: A");
+    else if (marks >= 70)
+        printf("Grade: B");
+    else if (marks >= 60)
+        printf("Grade: C");
+    else if (marks >= 40)
+        printf("Grade: D");
+    else
+        printf("Grade: Fail");
+    return 0;
+}
