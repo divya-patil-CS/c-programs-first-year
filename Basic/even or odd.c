af9ee8b304d99e1e44a634d3625abc250a95c452
divya-patil-CS <divyaoffcial33@gmail.com>
2026-01-02 20:08:05 +0530

    Update even or odd.c

--- Basic/even or odd.c
+++ Basic/even or odd.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 int main() {
     int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    if (num % 2 == 0)
+    printf("Enter a number:");
+    scanf("%d",&num);
+    if (num%2==0)
      printf("Even number");
     else
      printf("Odd number");
