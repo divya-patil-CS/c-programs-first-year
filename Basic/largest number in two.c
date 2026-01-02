4cf92975cdbd39450294dbcbbfa42ae2520ce866
divya-patil-CS <divyaoffcial33@gmail.com>
2026-01-02 20:10:23 +0530

    Update largest number in two.c

--- Basic/largest number in two.c
+++ Basic/largest number in two.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-int main() {
-    int a, b;
-    printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
-
-    if (a > b)
-     printf("Largest number is %d", a);
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter two numbers:");
+    scanf("%d %d",&a,&b);
+    if (a>b)
+     printf("Largest number is %d",a);
     else
-     printf("Largest number is %d", b);
+     printf("Largest number is %d",b);
     return 0;
 }
