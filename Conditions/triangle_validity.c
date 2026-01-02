781e01352f49184bc870d9e40625f97eec88eeac
divya-patil-CS <divyaoffcial33@gmail.com>
2026-01-02 20:03:24 +0530

    Update triangle_validity.c

--- Conditions/triangle_validity.c
+++ Conditions/triangle_validity.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main() {
-    int a, b, c;
-    printf("Enter three sides of triangle: ");
-    scanf("%d %d %d", &a, &b, &c);
-    if (a + b > c && a + c > b && b + c > a)
+    int a,b,c;
+    printf("Enter three sides of triangle:");
+    scanf("%d %d %d",&a,&b,&c);
+    if (a+b>c &&a+c>b &&b+c>a)
         printf("Valid Triangle");
     else
         printf("Invalid Triangle");
