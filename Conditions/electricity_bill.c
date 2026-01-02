9401ddce21f2a60d34ed9cfeb7d3193513a56a9e
divya-patil-CS <divyaoffcial33@gmail.com>
2026-01-02 19:59:47 +0530

    Update electricity_bill.c

--- Conditions/electricity_bill.c
+++ Conditions/electricity_bill.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
-int main() {
+#include<stdio.h>
+int main(){
     int units;
     float bill;
-    printf("Enter electricity units: ");
-    scanf("%d", &units);
-    if (units <= 50)
-        bill = units * 0.50;
-    else if (units <= 150)
-        bill = 50 * 0.50 + (units - 50) * 0.75;
-    else if (units <= 250)
-        bill = 50 * 0.50 + 100 * 0.75 + (units - 150) * 1.20;
+    printf("Enter electricity units:");
+    scanf("%d",&units);
+    if(units<=50)
+        bill=units*0.50;
+    else if(units<=150)
+        bill=50*0.50+(units-50)*0.75;
+    else if(units<=250)
+        bill=50*0.50+100*0.75+(units- 150)*1.20;
     else
-        bill = 50 * 0.50 + 100 * 0.75 + 100 * 1.20 + (units - 250) * 1.50;
-    bill = bill + (bill * 0.20); // 20% surcharge
-    printf("Total electricity bill = %.2f", bill);
+        bill=50*0.50+100*0.75+100*1.20+ (units-250)*1.50;
+    bill=bill+(bill*0.20);// 20% surcharge
+    printf("Total electricity bill=%.2f",bill);
     return 0;
 }
