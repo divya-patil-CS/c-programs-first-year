5093476816548b6abf32d98185018a82fb6049f2
divya-patil-CS <divyaoffcial33@gmail.com>
2026-01-06 21:47:02 +0530

    Create palindrome.c

---
+++ Loops/palindrome.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int num,original,reverse=0;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    original=num;
+
+    while(num!=0) {
+        reverse=reverse l*10+(num%10);
+        num=num/10;
+    }
+
+    if(original==reverse)
+        printf("Palindrome number");
+    else
+        printf("Not a palindrome");
+    return 0;
+}
